221b537a17266ef06ec17d910651975bbe7cd910
ArtanisTheOne <52015929+ArtanisTheOne@users.noreply.github.com>
2020-06-09 19:20:58 -0400

    Update message.js

--- src/message.js
+++ src/message.js
@@ -17,7 +17,7 @@ module.exports = function(config, message, edited, deleted)
    //
    
    
-   if ((message.isAdmin)(message.member.permissions.has("ADMINISTRATOR"))
+   if ((message.isAdmin)(message.member.permissions.has("ADMINISTRATOR")))
    {
       return
    }
