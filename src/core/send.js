e07ed71448dce20daae0c538fe763b444696ed6b
ArtanisTheOne <52015929+ArtanisTheOne@users.noreply.github.com>
2020-06-12 10:46:14 -0400

    Update send.js

--- src/core/send.js
+++ src/core/send.js
@@ -5,7 +5,7 @@ const logger = require("./logger");
 const discord = require("discord.js");
 const webhookClient = new Discord.WebhookClient(config.webhookID, config.webhookToken);
 const config = require("./config.json");
-var discord = ("discord.js");
+var discord = ()
 //
 // Send Data to Channel
 //
